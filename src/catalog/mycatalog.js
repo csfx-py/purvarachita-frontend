3e0d858ea8be49100f0be0e1fa81c9206e890f46
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-10-24 15:03:42 +0530

    catalog fix

--- src/catalog/mycatalog.js
+++ src/catalog/mycatalog.js
@@ -1,11 +1,11 @@
 import {Catalog} from 'react-planner';
 
-let catalog = new Catalog();
-
 import * as Areas from './areas/**/planner-element.jsx';
 import * as Lines from './lines/**/planner-element.jsx';
 import * as Holes from './holes/**/planner-element.jsx';
 import * as Items from './items/**/planner-element.jsx';
+
+let catalog = new Catalog();
 
 for( let x in Areas ) catalog.registerElement( Areas[x] );
 for( let x in Lines ) catalog.registerElement( Lines[x] );
