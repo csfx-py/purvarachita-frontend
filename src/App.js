972987889ea2d0dec9d2a661d392d3be25c6b76f
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-03 15:28:46 +0530

    added appbar to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Builder from "./Builder";
+import Bar from "./Components/Bar";
 
 function App() {
   return (
     <div className="App">
+      <Bar />
       <Builder />
     </div>
   );
