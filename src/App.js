b8b877ddd7ff3a8ce6318a600f60846aa5a56833
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-30 02:23:06 +0530

    pulled routes from mentor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,64 @@
+import { Box } from "@mui/material";
+import { useContext } from "react";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
+// import Err404 from "./Components/Err404";
+import Loading from "./Components/Loading";
+import Nav from "./Components/Nav";
+import { FeedProvider } from "./Contexts/FeedContext";
+import { LoadingContext } from "./Contexts/LoadingContext";
+import { UserContext } from "./Contexts/UserContext";
+import Auth from "./Routes/Auth";
+import Feed from "./Routes/Feed";
+import Home from "./Routes/Home";
+import Profile from "./Routes/Profile";
 import Builder from "./Routes/Builder";
-import {  Routes, Route } from "react-router-dom";
 
 function App() {
+  const { loading } = useContext(LoadingContext);
+  const { user, userData } = useContext(UserContext);
+
+  // get path from url
+  const { pathname, state } = useLocation();
+
   return (
     <div className="App">
-        <Routes>
-          <Route path="/" element={<h1>Hello</h1>} />
-          <Route path="/build" element={<Builder />} />
-        </Routes>
+      {loading && (
+        // loading screen absolute position
+        <Loading />
+      )}
+      <FeedProvider userData={userData}>
+        <Box>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/auth"
+              element={user ? <Navigate to={state} /> : <Auth />}
+            />
+            <Route
+              path="/community"
+              element={
+                user ? <Feed /> : <Navigate to="/auth" state={pathname} />
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                user ? <Profile /> : <Navigate to="/auth" state={pathname} />
+              }
+            />
+            <Route
+              path="/build"
+              element={
+                user ? <Builder /> : <Navigate to="/auth" state={pathname} />
+              }
+            />
+            {/* <Route path="*" element={<Err404 />} /> */}
+            <Route path="*" element={<h1>404</h1>} />
+          </Routes>
+        </Box>
+      </FeedProvider>
     </div>
   );
 }
