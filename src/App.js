ebbc26ef6c34e091869d190b319d8e47a0b5005c
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-30 04:19:53 +0530

    added 404

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Box } from "@mui/material";
 import { useContext } from "react";
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
-// import Err404 from "./Components/Err404";
 import Loading from "./Components/Loading";
 import Nav from "./Components/Nav";
 import { FeedProvider } from "./Contexts/FeedContext";
@@ -13,6 +12,7 @@ import Feed from "./Routes/Feed";
 import Home from "./Routes/Home";
 import Profile from "./Routes/Profile";
 import Builder from "./Routes/Builder";
+import Err from "./Routes/Err";
 
 function App() {
   const { loading } = useContext(LoadingContext);
@@ -54,8 +54,7 @@ function App() {
                 user ? <Builder /> : <Navigate to="/auth" state={pathname} />
               }
             />
-            {/* <Route path="*" element={<Err404 />} /> */}
-            <Route path="*" element={<h1>404</h1>} />
+            <Route path="*" element={<Err />} />
           </Routes>
         </Box>
       </FeedProvider>
